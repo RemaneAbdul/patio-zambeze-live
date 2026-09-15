4cc299666188601f1e6e99fd27e58e5e20912ee1
remane <yuranremane51@gmail.com>
2026-09-15 16:39:27 +0200

    chore: remove serverless tRPC adapter

--- api/trpc/[...path].ts
+++
@@ -1,7 +0,0 @@
-import { createApiApp } from "../../server/_core/app";
-
-// Use the same source application as the top-level API entrypoint so nested
-// tRPC procedures never execute a stale checked-in bundle in Vercel.
-const app = createApiApp();
-
-export default app;
